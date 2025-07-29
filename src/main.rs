7217f800f0168b74f91b263c6783b08c14aba4fc
Steven Rudenko <srudenko@ncl.com>
2025-07-29 18:35:48 +0300

    Add sorters

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ use std::path::{Path, PathBuf};
 
 use clap::Parser;
 use rust_sql_organizer::searcher::{get_all_files, EmptyFileExtensionError, FileExtension};
+use rust_sql_organizer::sorter::{Key, SORTING_STRATEGIES};
 
 #[derive(Clone, Debug)]
 enum CliError {
     ExtensionError,
     FileError,
+    SortingStratError,
 }
 
 #[derive(Parser)]
@@ -15,8 +17,12 @@ struct Cli {
     path: Option<PathBuf>,
 
     /// File extensions that will be used
-    #[arg(short = 'e', long)]
+    #[arg(short, long)]
     extension: Option<Vec<String>>,
+
+    /// Sorting Strategy. Default: first_number, folder
+    #[arg(short, long)]
+    sorter: Option<Vec<String>>,
 }
 
 fn main() -> Result<(), CliError> {
@@ -32,10 +38,25 @@ fn main() -> Result<(), CliError> {
         Err(_) => return Err(CliError::ExtensionError),
     };
     let path = args.path.unwrap_or(Path::new(".").to_path_buf());
-    let files = match get_all_files(&path, &extensions) {
+    let mut files = match get_all_files(&path, &extensions) {
         Ok(files) => files,
         Err(_) => return Err(CliError::FileError),
     };
+    let sorting_strats: Vec<&fn(&PathBuf) -> Key> = match args
+        .sorter
+        .unwrap_or(vec!["folder".to_string(), "first_number".to_string()])
+        .iter()
+        .map(|strat| SORTING_STRATEGIES.get(strat))
+        .rev()
+        .collect()
+    {
+        Some(strats) => strats,
+        None => return Err(CliError::SortingStratError),
+    };
+
+    for sorting_start in sorting_strats {
+        files.sort_by_key(|path| sorting_start(path));
+    }
     for file in files {
         println!("File: {:?}", file)
     }
---
+++ src/sorter/mod.rs
@@ -0,0 +1,105 @@
+use phf::{phf_map, Map};
+use regex::Regex;
+use std::{cmp::Ordering, path::PathBuf};
+
+use lazy_static::lazy_static;
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum Key {
+    Num(i32),
+    Text(String),
+}
+
+impl Ord for Key {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match (self, other) {
+            (Key::Num(a), Key::Num(b)) => a.cmp(b),
+            (Key::Text(a), Key::Text(b)) => a.cmp(b),
+            (Key::Num(_), Key::Text(_)) => Ordering::Less,
+            (Key::Text(_), Key::Num(_)) => Ordering::Greater,
+        }
+    }
+}
+
+impl PartialOrd for Key {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+type OrdFn = fn(&PathBuf) -> Key;
+
+lazy_static! {
+    static ref NUMBER_RE: Regex = Regex::new("[0-9]+").unwrap();
+    pub static ref SORTING_STRATEGIES: Map<&'static str, OrdFn> = phf_map! {
+        "first_number" => |path| {
+            let file_name = path.file_name().unwrap().to_str().unwrap_or("");
+            let num: i32 = match NUMBER_RE.find(file_name) {
+                Some(m) => m.as_str().parse().unwrap(),
+                None => 0,
+            };
+            Key::Num(num)
+        },
+        "last_number" => |path| {
+            let file_name = path.file_name().unwrap().to_str().unwrap_or("");
+            let num: i32 = match NUMBER_RE.find_iter(file_name).last() {
+                Some(m) => m.as_str().parse().unwrap(),
+                None => 0,
+            };
+            Key::Num(num)
+        },
+        "folder" => |path| {
+            let last_folder_name = match path.parent() {
+                Some(p) => match p.file_name() {
+                    Some(name) => name.to_str().unwrap(),
+                    None => return Key::Num(0),
+                },
+                None => return Key::Num(0),
+            };
+            Key::Text(last_folder_name.to_string())
+        }
+    };
+}
+
+#[cfg(test)]
+mod test_sorters {
+    use std::path::Path;
+
+    use super::{Key, SORTING_STRATEGIES};
+
+    #[test]
+    fn test_first_number() {
+        let first_number_strat = SORTING_STRATEGIES.get("first_number").unwrap();
+        assert_eq!(
+            first_number_strat(&Path::new("folder/100_deploy_20.sql").to_path_buf()),
+            Key::Num(100)
+        );
+    }
+
+    #[test]
+    fn test_last_number() {
+        let strat = SORTING_STRATEGIES.get("last_number").unwrap();
+        assert_eq!(
+            strat(&Path::new("folder/100_deploy_20.sql").to_path_buf()),
+            Key::Num(20)
+        );
+    }
+
+    #[test]
+    fn test_folder() {
+        let strat = SORTING_STRATEGIES.get("folder").unwrap();
+        assert_eq!(
+            strat(&Path::new("folder/100_deploy_20.sql").to_path_buf()),
+            Key::Text("folder".to_string())
+        )
+    }
+
+    #[test]
+    fn test_folder_no_folder() {
+        let strat = SORTING_STRATEGIES.get("folder").unwrap();
+        assert_eq!(
+            strat(&Path::new("/100_deploy_20.sql").to_path_buf()),
+            Key::Num(0)
+        )
+    }
+}
